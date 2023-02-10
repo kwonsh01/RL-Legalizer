e094bb25605e8462769969e87b4ec73211212ed4
kwonsh01 <shyun010302@postech.ac.kr>
2023-02-10 21:08:35 +0900

    first commit

--- cmake-build-debug-csdl-ubuntu/submodule/OpenDP/CMakeFiles/opendplib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libopendp.a"
-)
--- cmake-build-debug-csdl-ubuntu/submodule/OpenDP/module/def/5.8-p027/CMakeFiles/DefBisonTarget.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/intern_shkwon/tmp/tmp.eyv3ldFFuv/submodule/OpenDP/module/def/5.8-p027/def/def.tab.cpp"
-  "CMakeFiles/DefBisonTarget"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DefBisonTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
